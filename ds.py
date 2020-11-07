6e8d64e4d8c743ff735d24486bd423ef3974fb6b
vivek-nagre <68245772+vivek-nagre@users.noreply.github.com>
2020-11-07 18:02:53 +0530

    Add files via upload
    first day all assignment

---
+++ ds.py
@@ -0,0 +1,26 @@
+print("question no :1")
+print()
+print("random word answer --> RAINBOW")
+print()
+print("question two")
+x="lets upgrade"
+print(x.upper())
+print()
+print("quetion number three")
+print()
+
+cost_price=float(input("Cost Price:"))
+sell_price=float(input("selling Price:"))
+
+if cost_price<sell_price:
+    print("profit")
+elif cost_price==sell_price:
+    print("social work")
+else:
+    print("loss")
+print()
+print()
+print("question number 5")
+ind_curr=float(input())
+Euro=ind_curr*80
+print(Euro)
